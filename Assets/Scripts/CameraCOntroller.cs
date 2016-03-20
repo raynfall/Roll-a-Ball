b386f2258ad12bf4e97fa45219e0789a01d38582
raynfall <raymond.tran1204@gmail.com>
2016-03-20 12:07:07 -0500

    Camera and Walls
    Added camera movement and walls

---
+++ Assets/Scripts/CameraCOntroller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraCOntroller : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame but guaranteed after all items have been processed in update
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
