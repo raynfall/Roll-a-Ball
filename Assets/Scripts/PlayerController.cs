ea9c0734d360c51df4ed2266b25c629419d8181d
raynfall <raymond.tran1204@gmail.com>
2016-03-20 13:58:59 -0500

    UI and Build
    Finished up UI score and Build

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	
 	//Go to doc - CMD + '
 	public float speed;
+	public Text countText;
+	public Text winText;
+
 	private Rigidbody rb;
+	private int count;
 
 	void Start() {
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setCountText ();
+		winText.text = "";
 	}
 
 	void FixedUpdate() {
@@ -18,5 +26,18 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce (movement * speed);
+	}
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Pick Up")) {
+			other.gameObject.SetActive(false);
+			count++;
+			setCountText ();
+		}
+	}
+	void setCountText () {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You Win!";
+		}
 	}
 }
